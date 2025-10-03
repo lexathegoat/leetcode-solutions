757094b19ba489c352b749a73851fe90e8e3a81a
onur <lexaforsteam@gmail.com>
2025-10-03 12:17:44 -0700

    Create 407. Trapping Rain Water II.py

---
+++ 407. Trapping Rain Water II.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    def trapRainWater(self, heightMap):
+        if not heightMap or not heightMap[0]:
+            return 0
+
+        m, n = len(heightMap), len(heightMap[0])
+        visited = [[False] * n for _ in range(m)]
+        heap = []
+        for i in range(m):
+            for j in range(n):
+                if i in (0, m - 1) or j in (0, n - 1):
+                    heapq.heappush(heap, (heightMap[i][j], i, j))
+                    visited[i][j] = True
+        
+        res = 0
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        while heap:
+            height, x, y = heapq.heappop(heap)
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    res += max(0, height - heightMap[nx][ny])
+                    heapq.heappush(heap, (max(height, heightMap[nx][ny]), nx, ny))
+        
+        return res
